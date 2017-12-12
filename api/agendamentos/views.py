e002465b3ea92d2f487d5a89b80a2bea312600f5
elton jefferson <eltonjncorreia@gmail.com>
2017-12-12 09:09:13 -0300

    documentando mais a API

--- api/agendamentos/views.py
+++ api/agendamentos/views.py
@@ -8,8 +8,29 @@ from api.agendamentos.serializers import AgendamentoSerializers
 class AgendamentosViewSet(viewsets.ModelViewSet):
     """
 
-    Esta view fornece automaticamente `list`,` create`, `retrieve`,
+    Esta API fornece automaticamente `list`,` create`, `retrieve`,
     ações de "atualização" e "destruir".
+
+    retrieve:
+    Recupera os detalhes de um agendamento
+    URL http://localhost:8000/agendamentos/detalhes/1/
+
+    list:
+    Retorna uma lista de todos os agendamentos.
+    URL http://localhost:8000/agendamentos/
+
+    create:
+    Cria um novo agendamento.
+    URL http://localhost:8000/agendamentos/agendar/
+
+    update:
+    Atualiza um agendamento
+    URL http://localhost:8000/agendamentos/remarcar/1/
+
+    destroy:
+    Excluir um agendamento
+    URL http://localhost:8000/agendamentos/cancelar/1/
+
 
     """
     queryset = Agendamento.objects.all()
